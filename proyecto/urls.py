6165ea17be48962296afb1006ac0823ca1bd2fd9
SebastianBetancourt <joan.betancourt@correounivalle.edu.co>
2020-11-24 21:30:15 -0500

    using only class-based views

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
@@ -7,9 +7,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    url(r'home/^$', views.home, name='home'),
-    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^oauth/', include('social_django.urls', namespace='social')), 
-    url(r'^admin/', admin.site.urls),
+    path('home/', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('oauth/', include('social_django.urls', namespace='social'))
 ]
