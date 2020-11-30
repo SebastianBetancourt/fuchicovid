7b77efc532a6a3ec9fd494652d8171503a7462a5
SebastianBetancourt <joan.betancourt@correounivalle.edu.co>
2020-11-30 16:37:25 -0500

    test

--- fuchicovid/settings.py
+++ fuchicovid/settings.py
@@ -88,10 +88,12 @@ WSGI_APPLICATION = 'fuchicovid.wsgi.application'
 # variables de ambiente
 
 if 'IS_HEROKU' in os.environ:
-    print('is heroku')
     GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
     LD_LIBRARY_PATH = os.getenv('LD_LIBRARY_PATH')
     GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+    print(GDAL_LIBRARY_PATH)
+    print(LD_LIBRARY_PATH)
+    print(GEOS_LIBRARY_PATH)
 
 if 'DATABASE_URL' not in os.environ:
     import environ
